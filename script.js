31bf8d0d0a0d388b112198be0a8b354306e7604f
pd6974 <pdolan17@gmail.com>
2023-11-06 15:59:50 -0600

    A few commits late really: Managed to get the grid display function into a module (with the help of Nhan), created a factory for the players that works fairly well, started working on the module for the flow of the game.  Can I share module prototypes between the board and flow?  How can I access the gameboard more easily for the flow module?

--- script.js
+++ script.js
@@ -1,41 +1,66 @@
-/*
-This code works: 
+const gameboard = (function () {
+    const board = ['E', 'E', 'X', 'E', 'E', 'E', 'E', 'X', 'O'];
 
-const gameboard = ['X', 'O', 'X', 'X', 'O', 'N', 'N', 'X', 'O'];
-
-
-function displayBoard() {
+    const displayBoard = () => {
         const divs = document.querySelectorAll(".grid");
-        console.log(gameboard.length)
+        for (i = 0; i < board.length; i++) {
+             divs[i].textContent = board[i];
+             divs[i].setAttribute('data-tile', i);
+        }
+    };
+  
+    return {
+    board,
+    displayBoard
+  };
+})();
 
-        for (i = 0; i < gameboard.length; i++) {
-            divs[i].textContent = gameboard[i];
+gameboard.displayBoard();
+console.log(gameboard);
+
+
+
+const Player = (name) => {
+    const symbol = ['X', 'O'];
+    let playerOne;
+    let playerTwo;
+    const giveName = () => {
+        if (playerOne == undefined) {
+            this.symbol = symbol[0];
+            console.log(`Player one, ${symbol[0]}, is ${name}`)
+        }
+        else {
+            this.symbol = symbol[1];
+             console.log(`Player two, ${symbol[1]}, is ${name}`)
         }
         
     }
-
-displayBoard();
-
-*/
-
-
-const board = (function () {
-    const gameboard = ['X', 'O', 'X', 'X', 'O', 'N', 'N', 'X', 'O'];
-
-    return function displayBoard() {
-        const divs = document.querySelectorAll(".grid");
-        for (i = 0; i < gameboard.length; i++) {
-            return divs[i].textContent = gameboard[i];
-        }
-}});
-
-
-board.displayBoard();
-
-
-// I want to create a player and assign them X or O -- how can I do that efficiently in this factory?
-function createPlayer (name) {
-    const playerName = name;
-    return { playerName };
+    return { giveName }
 }
 
+
+const jim = Player('Jim');
+jim.giveName();
+
+const flow = (function () {
+    const divs = document.querySelectorAll(".grid");
+    divs.forEach(divs => divs.addEventListener('click', () => 
+    {executeMove(divs);}));
+
+        const executeMove = (x) => {
+            console.log(x)
+            if(x.textContent == 'E') {
+                board[x.dataset.tile] = Player.symbol;
+                x.textContent = Player.symbol;
+                console.log(board);
+            } else {
+                console.log('Invalid Move');
+            }
+        }
+    
+        return { executeMove }
+    }
+
+)();
+
+flow.executeMove();
